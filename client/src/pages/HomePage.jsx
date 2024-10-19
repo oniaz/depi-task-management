084213dee410f85e011ed540ff68864cfb13d98b
boodyf2 <abdalrahman.vim@gmail.com>
2024-10-19 23:24:30 +0300

    removed the api calls for loading tasks temporarily untill marwan adds login/register functionallity

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,6 +11,38 @@ const HomePage = () => {
     const fetcher = useFetcher();
     const columnsWithFetcher = useMemo(() => columns(fetcher), [fetcher]);
 
+    // THIS IS JUST FOR TESTING OUT FILTERING
+    const TEST_TASKS = [
+        {
+            id: 1,
+            title: "Depi Final Project",
+            status: "Todo",
+            priority: "High",
+            category: "Work",
+        },
+        {
+            id: 2,
+            title: "Amr Amein yapping lecture",
+            status: "done",
+            priority: "Low",
+            category: "School",
+        },
+        {
+            id: 3,
+            title: "new task",
+            status: "in progress",
+            priority: "Medium",
+            category: "Personal",
+        },
+        {
+            id: 3,
+            title: "new task",
+            status: "Done",
+            priority: "Medium",
+            category: "Personal",
+        },
+    ];
+
     const isLoading =
         fetcher.state === "loading" || fetcher.state === "submitting";
 
@@ -18,7 +50,7 @@ const HomePage = () => {
         isLoading ? (
             <LoaderCircle className="h-32 w-32 mx-auto my-48 animate-spin" />
         ) : (
-            <TasksTable columns={columnsWithFetcher} data={tasks} />
+            <TasksTable columns={columnsWithFetcher} data={TEST_TASKS} />
         )
     ) : (
         <Navigate to="/login" />
