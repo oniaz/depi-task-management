75f9af707fb51e8817a90676000513692271ab24
oniaz <32391049+oniaz@users.noreply.github.com>
2024-10-20 18:03:53 +0300

    remove commented code

--- api-simplified/models/user.js
+++ api-simplified/models/user.js
@@ -17,19 +17,4 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// // Hash password before saving the user
-// userSchema.pre('save', async function (next) {
-//     if (!this.isModified('password')) {
-//         return next();
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-// });
-
-// // Compare password for login
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//     return await bcrypt.compare(enteredPassword, this.password);
-// };
-
 module.exports = mongoose.model('User', userSchema);
